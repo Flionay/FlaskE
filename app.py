6633059ee30b72ef68a6d731e81f1829da268645
AngYi <angyi_jq@163.com>
2021-10-23 12:05:36 +0800

    chapter3-b

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask,render_template
 from Config import Config
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
 app.config.from_object(Config)
-
+bootstrap = Bootstrap(app)  # 将应用实例传给构造函数
+m = Moment(app)
 
 @app.route("/")
 def index():
-    names = {
-        "basketball":23,
-        "mylist":[1,2,3]
-    }
-    return render_template("index.html",names=names)
+    utc = datetime.utcnow()
+    return render_template("user.html",current_time=utc)
 
+@app.errorhandler(404)
+def errorhandler(e):
+    return render_template('404.html'),404
 
-@app.route('/base2')
-def func_name():
-    return render_template('base2.html')
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
 app.run()
